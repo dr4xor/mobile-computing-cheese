1f42a73f7470e198d2ffb4cd84ce42b9df37ed81
Marco Salas <marco@salas.cloud>
2021-02-27 01:11:03 +0100

    Fix paths for github pages

--- js/index.js
+++ js/index.js
@@ -89,10 +89,10 @@ window.onload = function() {
     });
 
     Promise.all([
-        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+        faceapi.nets.tinyFaceDetector.loadFromUri('mobile-computing-cheese/models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('mobile-computing-cheese/models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('mobile-computing-cheese/models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('mobile-computing-cheese/models'),
     ]).then(initVideo);
 
     video.addEventListener('play', () => {
